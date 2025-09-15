30f244e711d64cb632cdaa382aaa196bb58e9a6b
Notex <josh@notex.app>
2025-09-15 18:02:19 +1000

    Add repo variable to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+REPO="${REPO:-glacier-modding/Hitman-Hashes}"
+
 echo "Downloading latest hash list..."
-curl -L https://github.com/glacier-modding/Hitman-Hashes/releases/latest/download/latest-hashes.7z -o latest-hashes.7z
+curl -L https://github.com/${REPO}/releases/latest/download/latest-hashes.7z -o latest-hashes.7z
 7z x latest-hashes.7z -y
 rm latest-hashes.7z
 
